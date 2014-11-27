2c0686f81b388b5b841d197a75cdc4515e3ceb38
Christian Lindgren <christian@crapht.se>
2014-11-28 00:32:06 +0900

    Added mean as a config
    Can now set the usage of mean feature from command line by passing
    -feature "mean".

--- src/edu/stanford/nlp/parser/nndep/DependencyParser.java
+++ src/edu/stanford/nlp/parser/nndep/DependencyParser.java
@@ -834,24 +834,25 @@ public class DependencyParser {
    */
   private DependencyTree predictInner(CoreMap sentence) {
     int numTrans = system.transitions.size();
-    
-    double[][] E1 = classifier.getE();
-    Double[][] E = new Double[E1.length][];
-    for(int i = 0; i < E1.length; i++) {
-      E[i] = new Double[E1[i].length];
-      for(int j = 0; j < E1[i].length; j++)
-        E[i][j] = E1[i][j];
-    }
-
     HashMap<Integer, Double[]> s = new HashMap<>();
+    
+    if (config.featureMean) {
+      double[][] E1 = classifier.getE();
+      Double[][] E = new Double[E1.length][];
+      for(int i = 0; i < E1.length; i++) {
+        E[i] = new Double[E1[i].length];
+        for(int j = 0; j < E1[i].length; j++)
+          E[i][j] = E1[i][j];
+      }
 
-    List<CoreLabel> labels = sentence.get(CoreAnnotations.TokensAnnotation.class);
-    int i = 0;
-    for (CoreLabel label : labels) {
-      Double[] embedding = Util.createMeanValueTweak(labels, i++, E1, this);
-      if (embedding != null) {
-        Integer id = getWordID(label.word());
-        s.put(id, embedding);
+      List<CoreLabel> labels = sentence.get(CoreAnnotations.TokensAnnotation.class);
+      int i = 0;
+      for (CoreLabel label : labels) {
+        Double[] embedding = Util.createMeanValueTweak(labels, i++, E1, this);
+        if (embedding != null) {
+          Integer id = getWordID(label.word());
+          s.put(id, embedding);
+        }
       }
     }
 
